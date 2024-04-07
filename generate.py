9ba294e8a8407804fe0dfeada817f23277368773
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-04-06 22:21:22 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -42,7 +42,7 @@ def load_model(filepath):
     from model import MusicTransformer
     from hparams import hparams
     
-    file = torch.load(filepath)
+    file = torch.load(filepath, map_location=torch.device("cpu"))
     if "hparams" not in file:
         file["hparams"] = hparams
 
